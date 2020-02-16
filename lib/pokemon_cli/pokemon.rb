6c99b90ad57e0adfcb29e986c21a29e93972630b
Jason Viola <jason.viola@mavs.uta.edu>
2020-02-16 02:58:49 +0000

    allows Pokemon class to initialize objects with variables and save

--- lib/pokemon_cli/pokemon.rb
+++ lib/pokemon_cli/pokemon.rb
@@ -1,3 +1,19 @@
 class Pokemon
+  @@all []
+  
+  attr_accessor :name, :id, :height, :weight, :types
+  
+  def initialize(name, id, height, weight, types)
+    @name = name
+    @id = id
+    @height = height
+    @weight = weight
+    @types = types
+    save
+  end
+  
+  def save
+    @@all << self
+  end
   
 end
