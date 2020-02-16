9aaab3e2541884993eab6224c490727ec0a48797
Jason Viola <jason.viola@mavs.uta.edu>
2020-02-16 03:36:56 +0000

    fixes parsed_response.each

--- lib/pokemon_cli/api.rb
+++ lib/pokemon_cli/api.rb
@@ -3,7 +3,7 @@ class API
   def find_pokemon
     url = "https://pokeapi.co/"
     response = HTTParty.get(url)
-    response.parsed_response_each do |pokemon|
+    response.parsed_response.each do |pokemon|
       name = pokemon["name"]
       id = pokemon["id"]
       height = pokemon["height"]
