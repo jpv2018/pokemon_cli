e56c458c868817f5ef49f48bcc75d61e3c1413a4
Jason Viola <jason.viola@mavs.uta.edu>
2020-02-16 22:36:26 +0000

    testing interaction with api

--- lib/pokemon_cli/api.rb
+++ lib/pokemon_cli/api.rb
@@ -1,16 +1,15 @@
 class API
 require 'pry'
-  def find_pokemon
-    url = "https://pokeapi.co/api/v2/pokemon"
+  def find_pokemon(name)
+    url = "https://pokeapi.co/api/v2/pokemon/#{name}"
     response = HTTParty.get(url)
-    response.parsed_response.each do |pokemon|
-      name = pokemon["pokemon-species/"]
-      id = pokemon["id"]
-      height = pokemon["height"]
-      weight = pokemon["weight"]
-      type = pokemon["types"]
-    
-    
+    response.parsed_response["results"].each do |pokemon|
+      name = pokemon["species"]
+      #id = pokemon["id"]
+     # height = pokemon["height"]
+      #weight = pokemon["weight"]
+      #type = pokemon["types"]
+    binding.pry
       Pokemon.new(name, id, height, weight, types)
     end
   end
