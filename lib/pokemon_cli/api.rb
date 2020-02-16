626ceb6476cbe5d0ea457a289b8382803332ebd1
Jason Viola <jason.viola@mavs.uta.edu>
2020-02-16 03:16:37 +0000

    adds the ability to parse responses and create pokemon with attributes

--- lib/pokemon_cli/api.rb
+++ lib/pokemon_cli/api.rb
@@ -4,5 +4,10 @@ def find_pokemon
   url = "https://pokeapi.co/"
   response = HTTParty.get(url)
   response.parsed_response_each do |pokemon|
-    
+    name = pokemon["name"]
+    id = pokemon["id"]
+    height = pokemon["height"]
+    weight = pokemon["weight"]
+    type = pokemon["type"]
+  end
 end
