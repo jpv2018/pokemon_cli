c35edac823138bd987f800e207907f16310796a5
Jason Viola <jason.viola@mavs.uta.edu>
2020-02-16 18:12:54 +0000

    some formatting for cli

--- lib/pokemon_cli/cli.rb
+++ lib/pokemon_cli/cli.rb
@@ -1,3 +1,25 @@
 class CLI
   
+  def start
+    puts "Let's find a Pokemon to learn about!"
+    run
+  end
+  
+  def run
+    puts "Would you like to continue?"
+    pokemon_names
+  end
+  
+  def pokemon_names
+    puts "pokemon_id. pokemon_name"
+    select_pokemon
+  end
+  
+  def select_pokemon
+    puts "pokemon_id. pokemon_name"
+    puts "height = pokemon_height inches"
+    puts "weight = pokemon_weight lbs"
+    puts "types = pokemon_types"
+  end
+  
 end
